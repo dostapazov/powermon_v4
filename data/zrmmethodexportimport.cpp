8d17afde2e9670af2d619cd702037d5f093befdf
Dmitry Ostapenko <dostapazov@gmail.com>
2022-02-20 19:38:22 +0300

    prepare to export/import

--- data/zrmmethodexportimport.cpp
+++ data/zrmmethodexportimport.cpp
@@ -7,12 +7,21 @@ ZrmMethodExportImport::ZrmMethodExportImport(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->zrmMethods->setAbstract(true);
+    initSlost();
 }
+
 
 ZrmMethodExportImport::~ZrmMethodExportImport()
 {
     delete ui;
 }
+
+void ZrmMethodExportImport::initSlost()
+{
+  connect(ui->tbExport, &QAbstractButton::clicked,this, &ZrmMethodExportImport::exportMethod);
+  connect(ui->tbImport, &QAbstractButton::clicked,this, &ZrmMethodExportImport::importMethod);
+}
+
 
 void ZrmMethodExportImport::setWorkMode(zrm::zrm_work_mode_t mode)
 {
@@ -33,5 +42,22 @@ void ZrmMethodExportImport::close_db()
 {
  ui->zrmMethods->close_database();
 }
+
+void ZrmMethodExportImport::importMethod()
+{
+
+}
+
+void ZrmMethodExportImport::exportMethod()
+{
+
+    zrm::zrm_method_t method;
+
+    ui->zrmMethods->get_method(method, ZrmBaseWidget::codec());
+    QListWidgetItem * item = new QListWidgetItem;
+    item->setText(ZrmBaseWidget::codec()->toUnicode(QByteArray(method.m_method.m_name,sizeof(method.m_method.m_name))));
+    ui->methodsList->addItem(item);
+}
+
 
 
