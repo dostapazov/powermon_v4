2716d1b4be0f1100269196fb966d951ab512ad04
Dmitry <dostapazov@gmail.com>
2022-07-16 18:19:43 +0300

    Write parameters

--- zrm/zrmparamsview.h
+++ zrm/zrmparamsview.h
@@ -4,6 +4,7 @@
 #include "ui_zrmparamsview.h"
 #include <zrmbasewidget.h>
 #include <qmap.h>
+#include <QIcon>
 
 
 
@@ -11,28 +12,32 @@ class ZrmParamsView : public ZrmChannelWidget, private Ui::ZrmParamsView
 {
     Q_OBJECT
 public:
-    enum     columns_t {column_name, column_value};
+    enum     columns_t : int {column_name, column_value, column_new_value};
 
     explicit ZrmParamsView(QWidget* parent = nullptr);
     void    channel_param_changed(unsigned channel, const zrm::params_list_t& params_list  ) override;
     void    clear_controls() override;
 
-protected slots:
+private slots:
     void    request ();
     void serviceMode();
+    void writeParameters();
+    void paramChanged(QTreeWidgetItem*, int column);
+    void passwdChanged(const QString& text);
 
-protected:
-    void init_params();
-    void appendParam(zrm::zrm_param_t, const QString& text, bool ordered);
-    using params_items_t  =  QMap<zrm::zrm_param_t, QTreeWidgetItem* >;
 private:
+    void init_params();
+    void appendParam(zrm::zrm_param_t, const QString& text, bool ordered, bool editable = false);
+    using params_items_t  =  QMap<zrm::zrm_param_t, QTreeWidgetItem* >;
     void onActivate() override;
     void onDeactivate() override;
     zrm::params_t    m_orders;
     zrm::params_t    m_query_parms;
     params_items_t   m_items;
+    QList<QTreeWidgetItem*> m_EditableItems;
     QTreeWidgetItem* respond = nullptr;
     QTimer           m_request_timer;
+    QIcon            m_EditableIcon;
 
 };
 
